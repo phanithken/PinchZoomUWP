c5e2177d0e282bb36788ca3d4522b0e4f9e62e26
phanithken <phanith.ken@quad.co.jp>
2019-08-20 16:20:06 +0900

    feat: init

---
+++ MyImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinchZoomUWP
+{
+    public class MyImage
+    {
+        public MyImage()
+        {
+        }
+
+        public MyImage(string uri)
+        {
+            this.Path = uri;
+        }
+
+        public string Path { get; set; }
+    }
+}
